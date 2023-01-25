57f0589f57c0f28094d1acb7ff068debef2833b8
Megyeri Márk Máté <megyko1234@gmail.com>
2023-01-25 10:10:33 +0100

    Kész a feladat

--- src/app/diamond/diamond.component.ts
+++ src/app/diamond/diamond.component.ts
@@ -10,11 +10,40 @@ export class DiamondComponent implements OnInit {
   aside = new FormControl('');
   alfa = new FormControl('');
   perimeter = new FormControl('');
-  area= new FormControl('');
+  area = new FormControl('');
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  oneKuyup(event: any) {
+    if (event.key == "Enter") {
+      this.onEnterHendler();
+
+    };
+
+
+  }
+  onEnterHendler() {
+    let aside = Number(this.aside.value);
+    let alfa = Number(this.alfa.value);
+    let perimeter = this.calcPerimeter(aside);
+    let area = this. calcArea(aside, alfa);
+    this.perimeter.setValue(String(perimeter));
+    this.area.setValue(String(area));
+  };
+
+  calcPerimeter(aside:number) {
+
+    return aside*4;
+
+  }
+  calcArea(aside:number ,alfa:number) {
+    let rad = alfa*Math.PI/180;
+    let area=Math.pow(aside,2 )* Math.sin(rad);
+    return area;
+
   }
 
 }
